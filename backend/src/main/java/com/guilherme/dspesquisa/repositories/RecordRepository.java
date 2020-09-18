7f8a2a04ac2943577688fb7345fd18baba419974
Guilherme Pinheiro <guilhermemarkevis@hotmail.com>
2020-09-18 09:35:37 -0300

    GET records

--- backend/src/main/java/com/guilherme/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/guilherme/dspesquisa/repositories/RecordRepository.java
@@ -1,5 +1,10 @@
 package com.guilherme.dspesquisa.repositories;
 
+import java.time.Instant;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +12,10 @@ import com.guilherme.dspesquisa.entities.Record;
 
 @Repository
 public interface RecordRepository extends JpaRepositoryImplementation<Record, Long>{
+
+	@Query("SELECT obj FROM Record obj WHERE"
+			+ "(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND "
+			+ "(coalesce(:max, null) IS NULL OR obj.moment <= :max)")
+	Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
 
 }
